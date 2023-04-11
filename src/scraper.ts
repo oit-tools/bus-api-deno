3309cf984470b1e420760749cc083e0e237a8bc6
kota <52403688+yashikota@users.noreply.github.com>
2023-04-11 18:21:50 +0900

    dow

--- src/scraper.ts
+++ src/scraper.ts
@@ -3,11 +3,18 @@ import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-w
 export const getBusInformation = async (urls: string[]) => {
     try {
         const res = await fetch(urls[0]);
-        const html = await res.text();
+
+        // Shift_JIS to UTF-8
+        const sjis = await res.arrayBuffer();
+        const decoder = new TextDecoder("shift_jis");
+        const text = decoder.decode(sjis);
+        const encoder = new TextEncoder();
+        const utf8 = encoder.encode(text);
+
+        const html = new TextDecoder("utf-8").decode(utf8);
         const doc = new DOMParser().parseFromString(html, "text/html")!;
 
-        const dow = doc.querySelector("font[color='red']")?.textContent;
-        console.log(dow);
+        const dow = doc.querySelectorAll("font")[1].textContent;
     } catch (error) {
         console.log(error);
     }
