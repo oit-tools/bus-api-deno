efa32a9a12c8b94930ae2d40173c8f44934d288c
kota <52403688+yashikota@users.noreply.github.com>
2023-04-11 12:00:26 +0900

    add type

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,6 +1,6 @@
 import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm.ts";
 
-export const getBusInformation = async (urls: any) => {
+export const getBusInformation = async (urls: string[]) => {
     try {
         const res = await fetch(urls[0]);
         const html = await res.text();
