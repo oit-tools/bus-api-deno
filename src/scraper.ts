cfb243a8c851582523fbfa68bdf22571dad59421
kota <52403688+yashikota@users.noreply.github.com>
2023-04-17 16:59:08 +0900

    add terminal, stop, timetable

--- src/scraper.ts
+++ src/scraper.ts
@@ -14,7 +14,38 @@ export const getBusInformation = async (urls: string[]) => {
         const html = new TextDecoder("utf-8").decode(utf8);
         const doc = new DOMParser().parseFromString(html, "text/html")!;
 
-        const dow = doc.querySelectorAll("font")[1].textContent;
+        // terminal, stop
+        const terminal = doc.textContent!.split("条件１：")[1].split("／")[0];
+        const stop = doc.textContent!.split("条件２：")[1].split("／")[0];
+
+        // day of week
+        // const dow = doc.querySelectorAll("font")[1].textContent;
+        // console.log(dow);
+
+        // timetable
+        const table = doc.querySelector("#datalist")!.textContent;
+        const timetable = table.split("\n").filter((s) => s !== "").map((s) => s.trim());
+
+        const busInformation = [];
+        for (let i = 0; i < timetable.length - 1; i += 4) {
+            let [time, type, destination] = timetable[i].split(/[\[\]]/).filter((s) => s.trim() !== "");
+
+            // remove space
+            time = time.trim();
+            destination = destination.trim();
+
+            const schedule = time.includes("<") ? time.split("<")[1].split(">")[0].replace("定刻：", "").replace(/<.*>/, "") : time;
+
+            busInformation.push({
+                terminal,
+                stop,
+                time,
+                schedule,
+                type,
+                destination
+            });
+        }
+        console.log(busInformation);
     } catch (error) {
         console.log(error);
     }
