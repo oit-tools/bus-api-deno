b6742235249dd0f224af62bbf7276f352ed4ecae
kota <52403688+yashikota@users.noreply.github.com>
2023-04-18 13:49:27 +0900

    response

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,52 +1,56 @@
 import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm.ts";
 
 export const getBusInformation = async (urls: string[]) => {
-    try {
-        const res = await fetch(urls[0]);
+    let bus: any[] = [];
+    for (const url of urls) {
+        try {
+            const res = await fetch(url);
 
-        // Shift_JIS to UTF-8
-        const sjis = await res.arrayBuffer();
-        const decoder = new TextDecoder("shift_jis");
-        const text = decoder.decode(sjis);
-        const encoder = new TextEncoder();
-        const utf8 = encoder.encode(text);
+            // Shift_JIS to UTF-8
+            const sjis = await res.arrayBuffer();
+            const decoder = new TextDecoder("shift_jis");
+            const text = decoder.decode(sjis);
+            const encoder = new TextEncoder();
+            const utf8 = encoder.encode(text);
 
-        const html = new TextDecoder("utf-8").decode(utf8);
-        const doc = new DOMParser().parseFromString(html, "text/html")!;
+            const html = new TextDecoder("utf-8").decode(utf8);
+            const doc = new DOMParser().parseFromString(html, "text/html")!;
 
-        // terminal, stop
-        const terminal = doc.textContent!.split("条件１：")[1].split("／")[0];
-        const stop = doc.textContent!.split("条件２：")[1].split("／")[0];
+            // terminal, stop
+            const terminal = doc.textContent!.split("条件１：")[1].split("／")[0];
+            const stop = doc.textContent!.split("条件２：")[1].split("／")[0];
 
-        // day of week
-        // const dow = doc.querySelectorAll("font")[1].textContent;
-        // console.log(dow);
+            // day of week
+            // const dow = doc.querySelectorAll("font")[1].textContent;
+            // console.log(dow);
 
-        // timetable
-        const table = doc.querySelector("#datalist")!.textContent;
-        const timetable = table.split("\n").filter((s) => s !== "").map((s) => s.trim());
+            // timetable
+            const table = doc.querySelector("#datalist")!.textContent;
+            const timetable = table.split("\n").filter((s) => s !== "").map((s) => s.trim());
 
-        const busInformation = [];
-        for (let i = 0; i < timetable.length - 1; i += 4) {
-            let [time, type, destination] = timetable[i].split(/[\[\]]/).filter((s) => s.trim() !== "");
+            const busInformation = [];
+            for (let i = 0; i < timetable.length - 1; i += 4) {
+                let [time, type, destination] = timetable[i].split(/[\[\]]/).filter((s) => s.trim() !== "");
 
-            // remove space
-            time = time.trim();
-            destination = destination.trim();
+                // remove space
+                time = time.trim();
+                destination = destination.trim();
 
-            const schedule = time.includes("<") ? time.split("<")[1].split(">")[0].replace("定刻：", "").replace(/<.*>/, "") : time;
+                const schedule = time.includes("<") ? time.split("<")[1].split(">")[0].replace("定刻：", "").replace(/<.*>/, "") : time;
 
-            busInformation.push({
-                terminal,
-                stop,
-                time,
-                schedule,
-                type,
-                destination
-            });
+                busInformation.push({
+                    terminal,
+                    stop,
+                    time,
+                    schedule,
+                    type,
+                    destination
+                });
+            }
+            bus = bus.concat(busInformation);
+        } catch (error) {
+            console.log(error);
         }
-        console.log(busInformation);
-    } catch (error) {
-        console.log(error);
     }
+    return bus;
 };
--- src/server.ts
+++ src/server.ts
@@ -5,34 +5,44 @@ import { serve } from "https://deno.land/std@0.154.0/http/server.ts";
 const PORT = 8080;
 
 const handler = async (req: Request) => {
+    let busInformation;
     if (req.method === "GET") {
         switch (new URL(req.url).pathname) {
             case "/oit-nagao":
-                return new Response(
-                    JSON.stringify(await getBusInformation(oitNagao())),
-                )
+                busInformation = await getBusInformation(oitNagao());
+                break;
             case "/nagao-oit":
-                return new Response(
-                    JSON.stringify(await getBusInformation(nagaoOit())),
-                )
+                busInformation = await getBusInformation(nagaoOit());
+                break;
             case "/oit-kuzuha":
-                return new Response(
-                    JSON.stringify(await getBusInformation(oitKuzuha())),
-                )
+                busInformation = await getBusInformation(oitKuzuha());
+                break;
             case "/kuzuha-oit":
-                return new Response(
-                    JSON.stringify(await getBusInformation(kuzuhaOit())),
-                )
+                busInformation = await getBusInformation(kuzuhaOit());
+                break;
             default:
                 return new Response("Not Found", {
                     status: 404,
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
                 });
         }
     } else {
         return new Response("Method Not Allowed", {
             status: 405,
+            headers: {
+                "Content-Type": "application/json",
+            },
         });
     }
+
+    return new Response(JSON.stringify(busInformation), {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
 };
 
 await serve(handler, { port: PORT });
