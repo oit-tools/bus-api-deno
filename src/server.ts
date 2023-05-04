3705b85d4ef61576f2b0029411da7a0cee113262
kota <52403688+yashikota@users.noreply.github.com>
2023-05-05 01:41:02 +0900

    fmt

--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,47 @@
 import { getBusInformation } from "./scraper.ts";
-import { oitNagao, nagaoOit, oitKuzuha, kuzuhaOit } from "./urls.ts";
+import { kuzuhaOit, nagaoOit, oitKuzuha, oitNagao } from "./urls.ts";
 import { serve } from "https://deno.land/std@0.154.0/http/server.ts";
 
 const PORT = 8080;
 
 const handler = async (req: Request) => {
-    let busInformation;
-    if (req.method === "GET") {
-        switch (new URL(req.url).pathname) {
-            case "/oit-nagao":
-                busInformation = await getBusInformation(oitNagao());
-                break;
-            case "/nagao-oit":
-                busInformation = await getBusInformation(nagaoOit());
-                break;
-            case "/oit-kuzuha":
-                busInformation = await getBusInformation(oitKuzuha());
-                break;
-            case "/kuzuha-oit":
-                busInformation = await getBusInformation(kuzuhaOit());
-                break;
-            default:
-                return new Response("Not Found", {
-                    status: 404,
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                });
-        }
-    } else {
-        return new Response("Method Not Allowed", {
-            status: 405,
-            headers: {
-                "Content-Type": "application/json",
-            },
+  let busInformation;
+  if (req.method === "GET") {
+    switch (new URL(req.url).pathname) {
+      case "/oit-nagao":
+        busInformation = await getBusInformation(oitNagao());
+        break;
+      case "/nagao-oit":
+        busInformation = await getBusInformation(nagaoOit());
+        break;
+      case "/oit-kuzuha":
+        busInformation = await getBusInformation(oitKuzuha());
+        break;
+      case "/kuzuha-oit":
+        busInformation = await getBusInformation(kuzuhaOit());
+        break;
+      default:
+        return new Response("Not Found", {
+          status: 404,
+          headers: {
+            "Content-Type": "application/json",
+          },
         });
     }
-
     return new Response(JSON.stringify(busInformation), {
-        status: 200,
-        headers: {
-            "Content-Type": "application/json",
-        },
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
+  } else {
+    return new Response("Method Not Allowed", {
+      status: 405,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 };
 
 await serve(handler, { port: PORT });
